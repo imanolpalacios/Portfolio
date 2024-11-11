7dfa0d32df821977f817ce23a3c59255b55dbf01
Imanol Palacios Alvarez <ipalacios@uade.edu.ar>
2024-11-11 01:34:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector("#nav");
+    const abrir = document.querySelector("#abrir");
+    const cerrar = document.querySelector("#cerrar");
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("visible");
+        abrir.style.display = "none";
+        cerrar.style.display = "block";
+    });
+
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("visible");
+        abrir.style.display = "block";
+        cerrar.style.display = "none";
+    });
+});
+
+
+
+
+
+
+
